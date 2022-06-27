18cb97537a1e8cca3872818cc598860a9cb1001e
Trevor Sears <trevors@knorrmarketing.com>
2022-06-27 17:04:30 -0400

    Added outline of JSDoc rules.

---
+++ jsdoc.js
@@ -0,0 +1,62 @@
+module.exports = {
+	
+	"plugins": [
+		"jsdoc"
+	],
+	
+	"rules": {
+		
+		"jsdoc/check-access": "off",
+		"jsdoc/check-alignment": "off",
+		"jsdoc/check-examples": "off",
+		"jsdoc/check-indentation": "off",
+		"jsdoc/check-line-alignment": "off",
+		"jsdoc/check-param-names": "off",
+		"jsdoc/check-property-names": "off",
+		"jsdoc/check-syntax": "off",
+		"jsdoc/check-tag-names": "off",
+		"jsdoc/check-types": "off",
+		"jsdoc/check-values": "off",
+		"jsdoc/empty-tags": "off",
+		"jsdoc/implements-on-classes": "off",
+		"jsdoc/match-description": "off",
+		"jsdoc/match-name": "off",
+		"jsdoc/multiline-blocks": "off",
+		"jsdoc/newline-after-description": "off",
+		"jsdoc/no-bad-blocks": "off",
+		"jsdoc/no-defaults": "off",
+		"jsdoc/no-missing-syntax": "off",
+		"jsdoc/no-multi-asterisks": "off",
+		"jsdoc/no-restricted-syntax": "off",
+		"jsdoc/no-types": "off",
+		"jsdoc/no-undefined-types": "off",
+		"jsdoc/require-asterisk-prefix": "off",
+		"jsdoc/require-description-complete-sentence": "off",
+		"jsdoc/require-description": "off",
+		"jsdoc/require-example": "off",
+		"jsdoc/require-file-overview": "off",
+		"jsdoc/require-hyphen-before-param-description": "off",
+		"jsdoc/require-jsdoc": "off",
+		"jsdoc/require-param-description": "off",
+		"jsdoc/require-param-name": "off",
+		"jsdoc/require-param-type": "off",
+		"jsdoc/require-param": "off",
+		"jsdoc/require-property": "off",
+		"jsdoc/require-property-description": "off",
+		"jsdoc/require-property-name": "off",
+		"jsdoc/require-property-type": "off",
+		"jsdoc/require-returns-check": "off",
+		"jsdoc/require-returns-description": "off",
+		"jsdoc/require-returns-type": "off",
+		"jsdoc/require-returns": "off",
+		"jsdoc/require-throws": "off",
+		"jsdoc/require-yields": "off",
+		"jsdoc/require-yields-check": "off",
+		"jsdoc/sort-tags": "off",
+		"jsdoc/tag-lines": "off",
+		"jsdoc/valid-types": "off",
+		
+		
+	}
+	
+}
