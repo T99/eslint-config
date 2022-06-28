a065baa0bc9a8f12ff873b891007794c9afcb58c
Trevor Sears <trevors@knorrmarketing.com>
2022-06-28 12:42:16 -0400

    Added an `.eslintrc.js` ESLint config file. So meta.

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+"use strict";
+
+module.exports = {
+	
+	root: true,
+	
+	env: {
+		
+		node: true,
+		
+	},
+	
+	parserOptions: {
+		
+		ecmaVersion: "latest",
+		
+	},
+	
+	"extends": [
+		
+		"./all.js",
+		
+	],
+	
+};
