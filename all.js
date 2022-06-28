cc56ff56cf418dfed1a928500bf8279e8ff04ccb
Trevor Sears <trevors@knorrmarketing.com>
2022-06-28 16:31:30 -0400

    Added base `parserOptions` to `all.js`.
    Alongside helpful comments.

--- all.js
+++ all.js
@@ -6,10 +6,41 @@
 
 module.exports = {
 	
+	// Indicates additional configuration options with which to parse files.
+	parserOptions: {
+		
+		// Use the latest ECMAScript version.
+		ecmaVersion: "latest",
+		
+		// Tell ESLint that we are using modules (the other option is 'script').
+		sourceType: "module",
+		
+		// Tell ESLint that reserved words cannot be used as identifiers.
+		allowReserved: false,
+		
+		// Indicate which extra ECMAScript features we would like to use.
+		ecmaFeatures: {
+			
+			// Disables the use of globally-scoped 'return' statements.
+			globalReturn: false,
+			
+			// Whether or not to implicitly enable global 'strict' mode.
+			impliedStrict: true,
+			
+			// Enable the use of JSX syntax.
+			jsx: true,
+			
+		},
+		
+	},
+	
+	// An array of ESLint configurations that are being extended by this config.
 	"extends": [
 		
+		// This package's core set of ESLint rules.
 		"./eslint-core.js",
 		
+		// Rules surrounding JSDocs (provided by `eslint-plugin-jsdoc`).
 		"./jsdoc.js",
 		
 	],
